1d265308c5d0d699d0956424be01d264b0ebc2a9
pabol <pgm118@alu.ua.es>
2022-09-15 00:46:04 +0200

    Obtención de ID por parámetro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const url = new URLSearchParams(window.location.search);
+  console.log(url);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to feeesuu.
         </p>
+        <p>{url.get("id")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
